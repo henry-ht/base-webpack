23117422ccd821a7f4db527ef590fc4f0cdbc5ab
henryht <henrylapps@gmail.com>
2019-04-30 18:56:19 -0500

    inicio

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+let randomstring = require("randomstring");
+let htmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: "./src/js/index.js",
+    output: {
+        path: __dirname + '/build',
+        filename:  randomstring.generate(24) + '.js'
+    },
+    plugins: [
+        new htmlWebPackPlugin({
+            template: './src/index.html'
+        })
+    ],
+    module: {
+        rules: [
+          {
+            test: /\.css$/,
+            use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+          },
+        ],
+      }
+}
